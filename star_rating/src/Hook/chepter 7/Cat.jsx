7fa75c45981aac015398d37c6cd60c925a412a14
kimhyomin <khm83029@t-win.kr>
2026-04-15 15:35:53 +0900

    save before next chapter

--- star_rating/src/Hook/chepter 7/Cat.jsx
+++ star_rating/src/Hook/chepter 7/Cat.jsx
@@ -7,9 +7,10 @@ const Cat = memo(({ name, meow = (f) => f }) => {
   return <h1 onClick={() => meow(name)}>{name}</h1>;
 });
 
-const RenderCatOnce = memo(Cat, () => true);
+const RenderCatOnce = memo(Cat, () => true)
+// 처음 마운트 될 때 말곤 평생동안 다시 그려지지 않음
 const AlwaysRenderCat = memo(Cat, () => false);
-
+// 랜더링 할때마다 무적권 다르다고 하는 것 => 사실상 쓸모 없뜸
 export const PureCat = memo(
   Cat,
   (prevProps, nextProps) => prevProps.name === nextProps.name,
